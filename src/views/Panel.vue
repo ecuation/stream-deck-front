a39fbe70d92b75e68547e02d0fc3af6aa2d4c18e
Daniel Carvajal <dcarvajal@kiteris.com>
2022-03-05 12:52:30 +0100

    WIP: very basic setup

---
+++ src/views/Panel.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  mounted() {
+    console.log("hellot");
+  },
+});
+</script>
+<template>
+  <div class="container">
+    <div class="inline-flex rounded-md shadow-sm pt-6" role="group">
+      <button
+        type="button"
+        class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+      >
+        Stream ending {{ count }}
+      </button>
+      <button
+        type="button"
+        class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+      >
+        Be right back
+      </button>
+      <button
+        type="button"
+        class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+      >
+        Main Scene
+      </button>
+    </div>
+  </div>
+</template>
