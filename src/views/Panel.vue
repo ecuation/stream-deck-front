3a09ffbec6f28c294b9a07de1d5952e881de6f83
Daniel Carvajal <dcarvajal@kiteris.com>
2022-03-15 11:29:06 +0100

    basic panel test

--- src/views/Panel.vue
+++ src/views/Panel.vue
@@ -3,12 +3,46 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   data() {
-    return {
-      count: 1,
-    };
+    return {};
   },
-  mounted() {
-    console.log("hellot");
+
+  methods: {
+    changeScene(sceneName: string, mute: false) {
+      const data = {
+        instructions: {
+          setScene: sceneName,
+          mute: [
+            {
+              item: "Mic main",
+              status: mute,
+            },
+          ],
+        },
+      };
+      this.axios
+        .post("http://localhost:8081/api/obs/change-items", data)
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
+    streamEnding() {
+      const data = {
+        instructions: {
+          setScene: "Stream ending",
+          mute: [
+            {
+              item: "Mic main",
+              status: true,
+            },
+          ],
+        },
+      };
+      this.axios
+        .post("http://localhost:8081/api/obs/change-items", data)
+        .then((response) => {
+          console.log(response.data);
+        });
+    },
   },
 });
 </script>
@@ -18,20 +52,41 @@ export default defineComponent({
       <button
         type="button"
         class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        @click="changeScene('Starting soon', true)"
       >
-        Stream ending {{ count }}
+        Starting Soon
       </button>
+
       <button
         type="button"
         class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        @click="changeScene('Main scene', false)"
+      >
+        Main Scene
+      </button>
+
+      <button
+        type="button"
+        class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        @click="changeScene('FullScreen Scene', false)"
+      >
+        Fullscreen Scene
+      </button>
+
+      <button
+        type="button"
+        class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        @click="changeScene('Be right back', true)"
       >
         Be right back
       </button>
+
       <button
         type="button"
         class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        @click="streamEnding()"
       >
-        Main Scene
+        Stream ending
       </button>
     </div>
   </div>
